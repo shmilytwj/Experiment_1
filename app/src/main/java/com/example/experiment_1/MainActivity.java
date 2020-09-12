c3d49d7d1940bb322cb052332a5d220d52a527ec
shmilytwj <1272158921@qq.com>
2020-09-12 18:57:46 +0800

    个位数加减法

--- app/src/main/java/com/example/experiment_1/MainActivity.java
+++ app/src/main/java/com/example/experiment_1/MainActivity.java
@@ -14,11 +14,12 @@ import java.util.Stack;
 public class MainActivity extends AppCompatActivity {
     //使用栈存储数字和运算符
     Stack<Double> num = new Stack<>();
-    Stack<String> operate=new Stack<>();
-    final int NumID[]={R.id.Numbuer00,R.id.Numbuer0,R.id.Numbuer1,R.id.Numbuer2,R.id.Numbuer3
-            ,R.id.Numbuer4, R.id.Numbuer5,R.id.Numbuer6,R.id.Numbuer7,R.id.Numbuer8,R.id.Numbuer9};
-    final int OpID[]={R.id.Add,R.id.Sub,R.id.Multiplication,R.id.Division, R.id.Square,
-            R.id.Point,R.id.Prencent,R.id.LeftBracket,R.id.RightBracket};
+    Stack<String> operate = new Stack<>();
+    final int NumID[] = {R.id.Numbuer00, R.id.Numbuer0, R.id.Numbuer1, R.id.Numbuer2, R.id.Numbuer3
+            , R.id.Numbuer4, R.id.Numbuer5, R.id.Numbuer6, R.id.Numbuer7, R.id.Numbuer8, R.id.Numbuer9};
+    final int OpID[] = {R.id.Add, R.id.Sub, R.id.Multiplication, R.id.Division, R.id.Square,
+            R.id.Point, R.id.Prencent, R.id.LeftBracket, R.id.RightBracket};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,71 +28,133 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
 
-    int id=view.getId();
-    TextView text=(TextView)findViewById(R.id.DisplayScreen);
-    Button bt=(Button)findViewById(id);
-    for(int i=0;i<NumID.length;i++){
-        if (id == NumID[i]){
-            text.setText(text.getText().toString()+bt.getText().toString());
-           }
+        int id = view.getId();
+        TextView text = (TextView) findViewById(R.id.DisplayScreen);
+        Button bt = (Button) findViewById(id);
+        for (int i = 0; i < NumID.length; i++) {
+            if (id == NumID[i]) {
+                text.setText(text.getText().toString() + bt.getText().toString());
+            }
         }
-    for(int i=0;i<OpID.length;i++){
-        if(id==OpID[i]){
-            text.setText(text.getText().toString()+bt.getText().toString());
+        for (int i = 0; i < OpID.length; i++) {
+            if (id == OpID[i]) {
+                text.setText(text.getText().toString() + bt.getText().toString());
+            }
         }
-        }
-    if(id==R.id.Reset)
-        text.setText("");
+        if (id == R.id.Reset)
+            text.setText("");
 
-    if (id==R.id.Delete){
-        String string=text.getText().toString();
-        char [] str = string.toCharArray();
-        string="";
-        for(int i=0;i<str.length-1;i++)
-            string=string+str[i];
-        text.setText(string);
-    }
+        if (id == R.id.Delete) {
+            String string = text.getText().toString();
+            char[] str = string.toCharArray();
+            string = "";
+            for (int i = 0; i < str.length - 1; i++)
+                string = string + str[i];
+            text.setText(string);
+        }
     }
 
     public void AnsweronClick(View view) {
-        TextView text=(TextView) findViewById(R.id.DisplayScreen);
-        String string=text.getText().toString();
+        double Result = 0;
+        TextView text = (TextView) findViewById(R.id.DisplayScreen);
+        String string = text.getText().toString()+"#";
+        int n=0;
         //将显示屏中的表达式进行划分:数字和运算符
-        char [] str = string.toCharArray();
-        for(int i=0;i<str.length;i++){
-            if(str[i] <='9' && str[i]>='0'){
-                num.push(Double.parseDouble(String.valueOf(str[i])));
-            }
-            else if (str[i]=='+'| str[i]=='-' |str[i]=='x' |str[i]=='÷'| str[i]=='('|str[i]==')') {
-                if (operate.empty() && i!=str.length-1)
-                    operate.push(String.valueOf(str[i]));
-                else {
-                    String op = operate.pop();
+        char[] str = string.toCharArray();
+        operate.push("#");
+        //为运算符栈设置标志，仅剩一个元素#说明运算完毕
+        while (operate.peek()!="#" || n<str.length){
+            for (int i = 0; i < str.length; i++) {
+                if (str[i] <= '9' && str[i] >= '0') {
+                    num.push(Double.parseDouble(String.valueOf(str[i])));
+                } else if (str[i] == '+' | str[i] == '-' | str[i] == 'x' | str[i] == '÷' | str[i] == '(' | str[i] == ')'| str[i]=='#') {
                     //运算符的优先级判断
-                    if (compare(op, String.valueOf(str[i]))) {
-                        operate.push(op);
-                        operate.push(String.valueOf(str[i]));
-                    } else {
-                        Double num1 = num.pop();
-                        Double num2 = num.pop();
-
-                        Double Result = Calculator(num1, op, num2);
-                        num.push(Result);
-                        operate.push(String.valueOf(str[i]));
+                    switch (compare(operate.peek(), String.valueOf(str[i]))) {
+                        case "<":
+                            operate.push(String.valueOf(str[i]));
+                            break;
+                        case "=":
+                            operate.pop();
+                            break;
+                        case ">":
+                            String op = operate.pop();
+                            double b = num.pop();
+                            double a = num.pop();
+                            Result = Calculator(a, op, b);
+                            num.push(Result);
+                            break;
                     }
                 }
+                n = n + 1;
             }
-            else{
+            }
+        text.setText(String.valueOf(num.pop()));
+    }
 
+    private String compare(String op1, String op2) {
+        String op="";
+        switch (op1){
+            case "+":
+            case "-":
+                switch (op2){
+                    case "+":
+                    case "-":
+                    case "#":
+                    case ")":
+                        op=">";
+                        break;
+                    case "(":
+                    case "x":
+                    case "÷":
+                        op="<";
+                        break;
                 }
+                break;
+            case "x":
+            case "÷":
+                switch (op2){
+                    case "x":
+                    case "÷":
+                    case ")":
+                    case "#":
+                        op=">";
+                        break;
+                    case "(":
+                    case "+":
+                    case "-":
+                        op="<";
+                        break;
+                }
+                break;
+            case ")":
+            case "#":
+                op="<";
+                break;
+
         }
+        return op;
     }
 
-    private Double Calculator(Double pop, String op, Double pop1) {
-            return 0.0;
+    private double Calculator(double a, String op, double b) {
+        double result =0;
+        switch (op){
+            case "+":
+                result=a+b;
+                break;
+            case "-":
+                result=a-b;
+                break;
+            case "÷":
+                result=a/b;
+                break;
+            case "x":
+                result=a*b;
+                break;
+        }
+
+        return result;
     }
 
-    private boolean compare(String op, String valueOf) {
-            return true;
-    }
 }
+
+
